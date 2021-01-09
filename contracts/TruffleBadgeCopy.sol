33637bababa547f519a04d25255804eb869bfe89
michael chuang <mchuang484@gmail.com>
2021-01-09 16:05:45 -0500

    Test TruffleBadgeCopy 4

--- contracts/TruffleBadgeCopy.sol
+++ contracts/TruffleBadgeCopy.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract TruffleBadge is ERC721 {
+contract TruffleBadgeCopy is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _badgeIds;
 
@@ -15,7 +15,7 @@ contract TruffleBadge is ERC721 {
     /// @param attendee Attendee account address to whom the badge is to be awarded
     /// @param attendenceBadgeURI Uri to the badge metadata (https://ipfs.infura-ipfs.io/ipfs/QmP6AFJbppxmebAgsQJ6mNeBKe22VZGhMzBsSU143XfmiB/meta1.json)
     /// @return Autoincremented badge identifier via the counter utility
-    function awardBadge(address attendee, string memory attendenceBadgeURI)
+    function awardBadgeCopy(address attendee, string memory attendenceBadgeURI)
         public
         returns (uint256)
     {
@@ -29,7 +29,7 @@ contract TruffleBadge is ERC721 {
         return newBadgeId;
     }
 
-    function awardBadgeOrig()
+    function awardBadgeOrigCopy()
         public
         returns (uint256)
     {
